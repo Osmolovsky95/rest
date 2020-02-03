50422d22b67869a2d6490997d9e292d88da2eddd
A.Asmalouski <osmolovsky9595@gmail.com>
2020-02-03 12:43:33 +0300

    исправил QuestionController

--- src/main/java/controllers/QuestionController.java
+++ src/main/java/controllers/QuestionController.java
@@ -5,37 +5,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import service.QuestionService;
-import test.Context;
 
 @Controller
-@RequestMapping(value = "/questions")
 public class QuestionController {
+    private QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
 
     @RequestMapping(value = "/question",method = RequestMethod.POST,headers = {"Content-type=application/json"})
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public Question createQuestion(@RequestBody Question question) {
-        return Context.getInstance().getBean("questionService", QuestionService.class).createQuestion(question);
+      question =  questionService.createQuestion(question);
+      return question;
     }
 
     @RequestMapping(value = "/question/{id}",method = RequestMethod.GET,headers = {"Content-type=application/json"})
-    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public Question getQuestionJSON(@PathVariable String id ) {
-        return Context.getInstance().getBean("questionService", QuestionService.class).getQuestion(Long.parseLong(id));
+        return questionService.getQuestion(Long.parseLong(id));
     }
     @RequestMapping(value = "/question/{id}",method = RequestMethod.GET,headers = {"Content-type=application/xml"})
-    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public Question getQuestionXML(@PathVariable String id ) {
-        return Context.getInstance().getBean("questionService", QuestionService.class).getQuestion(Long.parseLong(id));
+        return questionService.getQuestion(Long.parseLong(id));
     }
 
     @RequestMapping(value = "/question/{id}",method = RequestMethod.DELETE,headers = {"Content-type=application/json"})
-    @ResponseStatus(HttpStatus.GONE)
     @ResponseBody
     public String deleteQuestion(@PathVariable String id) {
-        boolean result=Context.getInstance().getBean("questionService", QuestionService.class).deleteQuestion((Long.parseLong(id)));
+        boolean result=questionService.deleteQuestion((Long.parseLong(id)));
         if(result){
             return "OK";
         }
